1d5b99a2958846d19131132d5e1077d70cf61256
Ignacio Herrera <ignacioromanherrera@gmail.com>
2023-10-04 20:38:44 -0300

    Expo image picker

---
+++ ImagePickerAndRecognition/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Button, Image, View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function ImagePickerExample() {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={styles.mainView}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      <View style={styles.imageSections}>
+        {image && <Image source={{ uri: image }} style={styles.images} />}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageSections: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    justifyContent: 'center',
+  },
+  images: {
+    width: 250,
+    height: 250,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginHorizontal: 3,
+  },
+})
